5f06145895d7811452bc58fba8d3fbf1c487004e
ycseos <53217304+ycseos@users.noreply.github.com>
2025-09-20 15:09:20 +0900

    Update 하늘소 7번.py

--- 하늘소/하늘소 7번.py
+++ 하늘소/하늘소 7번.py
@@ -17,7 +17,8 @@ def solve():
         dist[b][a] = 1
 
     x, k = map(int, input().split())
-
+    
+    //플로이드 워셜
     for via in range(1, n+1):
         for i in range(1, n+1):
             for j in range(1, n+1):
@@ -32,3 +33,4 @@ def solve():
 
 if __name__ == "__main__":
     solve()
+
